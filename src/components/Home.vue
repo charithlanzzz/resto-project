0e56498ed717d46b2378cfed6b5268fef5cff9e7
charithlanzzz <lansakaracharith@gmail.com>
2022-11-15 17:52:38 +0530

    nav bar implemented

--- src/components/Home.vue
+++ src/components/Home.vue
@@ -1,11 +1,16 @@
 <template>
+    <Header />
     <h1>Hello user</h1>
 </template>
 
 <script>
+import Header from './Header.vue'
 export default{
     // eslint-disable-next-line vue/multi-word-component-names
     name:'Home',
+    components:{
+        Header
+    },
 
     mounted()
     {
