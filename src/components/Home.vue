d461b0fc2243eb39e08a373395c6ed9742c0f47e
charithlanzzz <lansakaracharith@gmail.com>
2022-11-16 12:41:15 +0530

    Delete Restaurant

--- src/components/Home.vue
+++ src/components/Home.vue
@@ -1,20 +1,23 @@
 <template>
-  <Header/>
-  <h1>Hello {{name}}, welcome to home page</h1>
+  <Header />
+  <h1>Hello {{ name }}, welcome to home page</h1>
   <table border="1px">
     <tr>
-        <td>ID</td>
-        <td>Name</td>
-        <td>Contact</td>
-        <td>Address</td>
-        <td>Action</td>
+      <td>ID</td>
+      <td>Name</td>
+      <td>Contact</td>
+      <td>Address</td>
+      <td>Action</td>
     </tr>
     <tr v-for="item in restaurant" :key="item.id">
-        <td>{{item.id}}</td>
-        <td>{{item.name}}</td>
-        <td>{{item.contact}}</td>
-        <td>{{item.address}}</td>
-        <td><router-link :to="'/update/'+item.id">Update</router-link></td>
+      <td>{{ item.id }}</td>
+      <td>{{ item.name }}</td>
+      <td>{{ item.contact }}</td>
+      <td>{{ item.address }}</td>
+      <td>
+        <router-link :to="'/update/' + item.id">Update</router-link>
+        <button v-on:click="deleteRestaurant(item.id)">Delete</button>
+      </td>
     </tr>
   </table>
 </template>
@@ -36,23 +39,33 @@ export default {
     Header,
   },
 
-  async mounted() {
-    let user = localStorage.getItem("user-info");
-    this.name = JSON.parse(user).name;
-    if (!user) {
-      this.$router.push({ name: "SignUp" });
-    }
+  methods: {
+    async deleteRestaurant(id) {
+      let result = await axios.delete("http://localhost:3000/restaurant/"+id);
+      if (result.status == 200) {
+        this.loadData();
+      }
+    },
 
-    let result = await axios.get("http://localhost:3000/restaurant");
-    console.warn(result);
-    this.restaurant = result.data;
+    async loadData() {
+      let user = localStorage.getItem("user-info");
+      this.name = JSON.parse(user).name;
+      if (!user) {
+        this.$router.push({ name: "SignUp" });
+      }
+
+      let result = await axios.get("http://localhost:3000/restaurant");
+      this.restaurant = result.data;
+    },
+  },
+  async mounted() {
+    this.loadData();
   },
 };
 </script>
 <style>
-td
-{
-    width:160px;
-    height: 40px;
+td {
+  width: 160px;
+  height: 40px;
 }
 </style>
